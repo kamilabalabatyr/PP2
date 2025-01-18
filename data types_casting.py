15cbfd044eb45914dfe0a00185d90f7217a92632
kamilabalabatyr <k_balabatyr@kbtu.kz>
2025-01-18 15:23:11 +0500

    casting

--- data types_casting.py
+++ data types_casting.py
@@ -75,3 +75,21 @@ print(type(c))
 import random
 
 print(random.randrange(1, 10))
+
+#Python Casting
+
+#1
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+#2
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+
+#3
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
+
