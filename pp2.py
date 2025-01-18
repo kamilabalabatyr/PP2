711e5f6fa14432c89cc7349c6f257b49589ace66
kamilabalabatyr <k_balabatyr@kbtu.kz>
2025-01-18 15:15:45 +0500

    Global Variables

--- pp2.py
+++ pp2.py
@@ -112,3 +112,44 @@ print(x + y)
 x = 5
 y = "John"
 print(x, y)
+
+#Python - Global Variables
+
+#1
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+#2
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+#3
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+
+#4 
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
